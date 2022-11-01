2dba0f562890d8669133292be3410384f3adafac
Lucas <lucas.rodrigues.ismart@gmail.com>
2022-11-01 13:15:51 -0300

    Commit da api telegram bot

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -124,6 +124,6 @@ def index():
         return '<h1> Rossmann Telegram Bot </h1>'
 
 if __name__ == '__main__':
-    port = os.environ('PORT', 5000)
+    port = os.environ.get('PORT', 5000)
     app.run(host='0.0.0.0', port=port)
 
